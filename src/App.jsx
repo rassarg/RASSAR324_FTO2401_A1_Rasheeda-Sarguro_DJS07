9472ed23f07c415254d91c6db387f3fa51b4ea70
rasheeda <raskieda@gmail.com>
2024-05-28 15:43:33 +0200

    Created Header component and added basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./components/Header.jsx";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
