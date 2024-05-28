14e00a3d15e985d34a686494804da9a85d38b6ba
rasheeda <raskieda@gmail.com>
2024-05-28 16:03:59 +0200

    updated Meme component and added google fonts

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -4,16 +4,26 @@ export default function Meme() {
   return (
     <main>
       <form className="form">
-        <input
-          type="text"
-          placeholder="Shut up"
-          className="form--input"
-        ></input>
-        <input
-          type="text"
-          placeholder="And take my money"
-          className="form--input"
-        ></input>
+        <div>
+          <label className="form--label">
+            Top Text
+            <input
+              type="text"
+              placeholder="Shut up"
+              className="form--input"
+            ></input>
+          </label>
+        </div>
+        <div>
+          <label className="form--label">
+            Bottom Text
+            <input
+              type="text"
+              placeholder="and take my money"
+              className="form--input"
+            ></input>
+          </label>
+        </div>
         <button className="form--button">Get a new meme image 🖼</button>
       </form>
     </main>
